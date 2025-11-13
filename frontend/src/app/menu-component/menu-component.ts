deaf5a8b8e8840fe92f4437327729d8c28f9f5db
Arturo <DaKingzz@users.noreply.github.com>
2025-11-13 13:24:57 -0500

    Component Error

--- frontend/src/app/menu-component/menu-component.ts
+++ frontend/src/app/menu-component/menu-component.ts
@@ -3,9 +3,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
 import { SearchPageComponent } from '../search-page-component/search-page-component';
 import { ViewTripsComponent } from '../view-trips-component/view-trips-component';
+import { ExcelReaderComponent } from '../excel-reader-component/excel-reader-component';
 @Component({
   selector: 'app-menu-component',
-  imports: [MatTabsModule, RouterModule, SearchPageComponent, ViewTripsComponent],
+  imports: [MatTabsModule, RouterModule, ExcelReaderComponent ,SearchPageComponent, ViewTripsComponent],
   templateUrl: './menu-component.html',
   styleUrl: './menu-component.css'
 })
